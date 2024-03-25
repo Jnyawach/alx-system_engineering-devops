28a303d39963722f1a2bc94343e46f558f56a833
jnyawach <nyawach41@gmail.com>
2024-03-25 10:18:23 -0700

    updated lists

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,36 +1,27 @@
 #!/usr/bin/python3
-"""python script to fetch Rest API for todo lists of employees"""
+"""Exports data in the JSON format"""
 
-import json
-import requests
-import sys
+if __name__ == "__main__":
 
+    import json
+    import requests
+    import sys
 
-if __name__ == '__main__':
-    url = "https://jsonplaceholder.typicode.com/users"
+    users = requests.get("https://jsonplaceholder.typicode.com/users")
+    users = users.json()
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+    todos = todos.json()
+    todoAll = {}
 
-    resp = requests.get(url)
-    Users = resp.json()
+    for user in users:
+        taskList = []
+        for task in todos:
+            if task.get('userId') == user.get('id'):
+                taskDict = {"username": user.get('username'),
+                            "task": task.get('title'),
+                            "completed": task.get('completed')}
+                taskList.append(taskDict)
+        todoAll[user.get('id')] = taskList
 
-    users_dict = {}
-    for user in Users:
-        USER_ID = user.get('id')
-        USERNAME = user.get('username')
-        url = 'https://jsonplaceholder.typicode.com/users/{}'.format(USER_ID)
-        url = url + '/todos/'
-        resp = requests.get(url)
-
-        tasks = resp.json()
-        users_dict[USER_ID] = []
-        for task in tasks:
-            TASK_COMPLETED_STATUS = task.get('completed')
-            TASK_TITLE = task.get('title')
-            users_dict[USER_ID].append({
-                "task": TASK_TITLE,
-                "completed": TASK_COMPLETED_STATUS,
-                "username": USERNAME
-            })
-            """A little Something"""
-    with open('todo_all_employees.json', 'w') as f:
-        json.dump(users_dict, f)
-
+    with open('todo_all_employees.json', mode='w') as f:
+        json.dump(todoAll, f)
